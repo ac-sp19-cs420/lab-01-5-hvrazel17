168d60ec81dc808a72d578763bded009e58e1a1d
Hagan M. Vrazel <hvrazel17@idc120-02.local>
2019-03-05 19:11:37 -0600

    Initial Commit

---
+++ Lab1_5hvrazel17/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lab1_5hvrazel17
+//
+//  Created by Hagan M. Vrazel on 3/5/19.
+//  Copyright © 2019 Hagan M. Vrazel. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
